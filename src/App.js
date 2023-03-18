35ba1a86bb863c9aba6e401c7b05de079e33bd93
IshwariyaManivanan <ishwariyavannan@gmail.com>
2023-03-19 00:33:07 +0530

    profile page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes  , Navigate} from 'react-router-dom';
 import Login from './components/login/login'
 import Signup from './components/signup/signup';
+import Profile from './components/profile/profile';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path='/'  element ={<Navigate to = "/login"/>}/>
       <Route path='login' element={<Login/>}/>
       <Route path='signup' element={<Signup/>}/>
+      <Route path='profile' element ={<Profile/>} />
     </Routes>
    </div>
   );
---
+++ src/components/profile/profile.js
@@ -0,0 +1,88 @@
+
+import React, { useState, useEffect } from "react";
+
+const Profile = () => {
+    const [user, setUser] = useState(null);
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("");
+    const [dob, setDob] = useState("");
+    const [mobile, setMobile] = useState("");
+
+    function handleSubmit() {
+
+    }
+    return (
+        <div className="main">
+            <div className="left">
+                <div className="leftMain">
+                    <div className="userName">
+                        <div>Name:</div>
+                        <div>{user}</div>
+                    </div>
+                    <div className="age">
+                        <div>
+                            Age:
+                        </div>
+                        <div>
+                            {age}
+                        </div>
+                    </div>
+                    <div className="gender">
+                        <div>
+                            Gender:
+                        </div>
+                        <div>
+                            {gender}
+                        </div>
+                    </div>
+                    <div className="dob">
+                        <div>
+                            Date Of Birth:
+                        </div>
+                        <div>
+                            {dob}
+                        </div>
+
+                    </div>
+                    <div className="mobile">
+                        <div>
+                            Mobile:
+                        </div>
+                        <div>
+                            {mobile}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="right">
+                <div className='profileForm'>
+                    <h1> Update Form</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className='age'>
+                            <label>Age:</label>
+                            <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+                        </div>
+                        <div className='gender'>
+                            <label>Gender:</label>
+                            <input type="text" value={gender} onChange={(e) => setGender(e.target.value)} />
+                        </div>
+                        <div className='dob'>
+                            <label>Date of Birth:</label>
+                            <input type="text" value={dob} onChange={(e) => setDob(e.target.value)} />
+                        </div>
+                        <div className='mobile'>
+                            <label>Mobile:</label>
+                            <input type="text" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+                        </div>
+                        <button type="submit">Update</button>
+                    </form>
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default Profile
